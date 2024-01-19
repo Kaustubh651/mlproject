a60aa47493f6bcd464c172263e09d626ff43e9a3
Kaustubhd651 <92288567+Kaustubhd651@users.noreply.github.com>
2024-01-19 22:51:41 +0530

    hyper parameter tunning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -16,65 +16,91 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object, evaluate_models
 
-from src.utils import save_object,evaluate_models
 
 @dataclass
-
 class ModelTrainerConfig:
-    trained_model_file_path = os.path.join("artifacts","model.pkl")
+    trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-
-
-
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
-            logging.info("Spilt training and test input data")
+            logging.info("Split training and test input data")
             X_train, y_train, X_test, y_test = (
                 train_array[:, :-1],
                 train_array[:, -1],
                 test_array[:, :-1],
                 test_array[:, -1],
             )
-
-            models={
-                "Random forest": RandomForestRegressor(),
+            models = {
+                "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
-                "Gradient Boosting":GradientBoostingRegressor(),
-                "Linear Regression":LinearRegression(),
-                "K-Neighbout": KNeighborsRegressor(),
-                "XGBClassier":XGBRegressor(),
-                "CatBossting Claasifier":CatBoostRegressor(verbose=False),
-                "Adaboost Classifier":AdaBoostRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "Linear Regression": LinearRegression(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            model_report: dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
 
+            model_report: dict = evaluate_models(
+                X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                models=models, params=params
+            )
 
-            # to get best model from the dict
-            best_model_score=max(sorted(model_report.values()))
+            # To get the best model score from the dict
+            best_model_score = max(sorted(model_report.values()))
 
-            ## To get best model name from the dict
-            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            # To get the best model name from the dict
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
 
-            best_model= models[best_model_name]
-
-            if best_model_score<0.6:
-                raise CustomException("No best model found")            
-            
+            if best_model_score < 0.6:
+                raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
-            ## preprocessor_obj = piclkle file
-
-            save_object(file_path=self.model_trainer_config.trained_model_file_path,
-                       obj =best_model
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
             )
-            predicted = best_model.predict(X_test)
-            r2 = r2_score(y_test,predicted)
 
-            return r2
+            predicted = best_model.predict(X_test)
+
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.exception import CustomException
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -24,13 +24,20 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,params):
     try:        
         ##print(f"Shapes: X_train={X_train.shape}, y_train={y_train.shape}, X_test={X_test.shape}, y_test={y_test.shape}")
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
 
             model.fit(X_train, y_train)  # train the model
 
