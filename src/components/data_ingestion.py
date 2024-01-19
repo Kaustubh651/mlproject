d575caf0cb06bd876ad0039240faa512a2675af3
Kaustubhd651 <92288567+Kaustubhd651@users.noreply.github.com>
2024-01-19 18:30:50 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,11 +1,19 @@
 import os 
 import sys
 import pandas as pd
+
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -49,4 +57,7 @@ if __name__ == '__main__':
     train_data, test_data = obj.initiate_Data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr,tes_arr,_=data_transformation.initiate_data_transformation(train_data, test_data)
+
+    modeltrainer =  ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,tes_arr))
--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path,obj):
@@ -22,3 +23,24 @@ def save_object(file_path,obj):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:        
+        ##print(f"Shapes: X_train={X_train.shape}, y_train={y_train.shape}, X_test={X_test.shape}, y_test={y_test.shape}")
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+
+            model.fit(X_train, y_train)  # train the model
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report  # Move the return statement outside the loop
+    except Exception as e:
+        raise CustomException(e, sys)
